65e88ee3c492b906d40f841d1b4367c732124a31
saadsulmi <saadsulmi@gmail.com>
2025-04-16 15:48:21 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1 className='anton-regular'>OFFRIFT</h1>
+        <h6 className='montserrat-sub2'>DRIFT DIFFERENT</h6>
+      </div>
+      <h1 className='montserrat-sub'>COMING SOON</h1>
+    </>
+  )
+}
+
+export default App
